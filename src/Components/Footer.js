f9168fb415b2bc76d495e1ade28fef8d765b250c
lawi <lawi.bett@student.moringaschool.com>
2023-08-03 14:00:16 +0300

    styled footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,10 @@
-// // src/components/Footer.js
-// import React from 'react';
 
-// const Footer = () => {
-//   const currentYear = new Date().getFullYear();
-
-//   return (
-//     <footer className="bg-gray-200 text-center py-2 mt-8">
-//       <p className="text-gray-600">
-//         &copy; {currentYear} Kitabu App. All rights reserved.
-//       </p>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 import React from 'react';
 
 const Footer = () => {
   return (
     <footer className="bg-black p-4 text-center text-white">
-      <p>&copy; {new Date().getFullYear()} My Book App. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Kitabu App. All rights reserved.</p>
     </footer>
   );
 };
