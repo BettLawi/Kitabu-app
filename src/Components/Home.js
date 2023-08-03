eb633f462751b95034341fd4958958125fd40785
lawi <lawi.bett@student.moringaschool.com>
2023-08-03 16:20:15 +0300

    styled everything

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Home() {
   return ( 
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-4">Welcome to My Book App</h1>
+      <h1 className="text-3xl font-bold mb-4">Welcome to Kitabu App</h1>
       <img
         src="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
         alt="Welcome"
